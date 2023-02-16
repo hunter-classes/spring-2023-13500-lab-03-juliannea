3d4f066ca3ea1492a5418ee46c2ba966150ceefb
juliannea <112399053+juliannea@users.noreply.github.com>
2023-02-16 11:36:23 -0500

    Add files via upload

---
+++ reservoir.h
@@ -0,0 +1,11 @@
+#pragma once 
+#include <string>
+#include <iostream>
+
+double get_east_storage(std::string);
+
+double get_min_east();
+
+double get_max_east();
+
+std::string compare_basins(std::string );
